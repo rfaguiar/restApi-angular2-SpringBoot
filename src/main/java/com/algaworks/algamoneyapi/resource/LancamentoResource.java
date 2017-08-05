ea974d21c466276aa3d9c3fdeef42fb86a204abd
Rogerio Aguiar <rfaguiar1@gmail.com>
2017-08-05 19:49:40 -0300

    5.3. Cadastrando o primeiro lançamento

--- src/main/java/com/algaworks/algamoneyapi/resource/LancamentoResource.java
+++ src/main/java/com/algaworks/algamoneyapi/resource/LancamentoResource.java
@@ -2,13 +2,21 @@ package com.algaworks.algamoneyapi.resource;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algamoneyapi.event.RecursoCriadoEvent;
 import com.algaworks.algamoneyapi.model.Lancamento;
 import com.algaworks.algamoneyapi.repository.LancamentoRepository;
 
@@ -18,6 +26,9 @@ public class LancamentoResource {
 
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
 
 	@GetMapping
 	public List<Lancamento> listar() {
@@ -32,5 +43,12 @@ public class LancamentoResource {
 		} else {
 			return ResponseEntity.notFound().build();
 		}
+	}
+	
+	@PostMapping
+	public ResponseEntity<Lancamento> criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
+		Lancamento lancamentoSalvo = this.lancamentoRepository.save(lancamento);
+		this.publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
 	}
 }
